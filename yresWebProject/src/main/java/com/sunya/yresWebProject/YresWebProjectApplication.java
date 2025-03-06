9d8c87229c00f8ef6ae05888652d73f55893c9bb
yres-c <sunyapong.c@gmail.com>
2025-03-06 19:51:40 +0700

    Converted the project from Spring to Spring Boot.

---
+++ yresWebProject/src/main/java/com/sunya/yresWebProject/YresWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sunya.yresWebProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YresWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YresWebProjectApplication.class, args);
+	}
+
+}
